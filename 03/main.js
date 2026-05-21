7ecda761752b930552ff76c89ff6f325739fd45d
alechos._.0k <alecsis012@hotmail.com>
2026-05-21 10:55:55 -0600

    actualizacion

--- 03/main.js
+++ 03/main.js
@@ -7,14 +7,34 @@ var anchoborde=document.getElementById("anchoborde");
 var colorborde=document.getElementById("colorborde");
 var tipoborde=document.getElementById("tipoborde");
 
+/* NUEVOS */
+var sombra=document.getElementById("sombra");
+var ejex=document.getElementById("ejex");
+var ejey=document.getElementById("ejey");
+
+/* FUNCION SOMBRA */
+function actualizarSombra(){
+
+    figura.style.boxShadow=
+    ejex.value+"px "+
+    ejey.value+"px "+
+    sombra.value+"px rgba(0,0,0,0.6)";
+
+    document.getElementById("vsombra").textContent=sombra.value;
+    document.getElementById("vejex").textContent=ejex.value;
+    document.getElementById("vejey").textContent=ejey.value;
+}
+
 alto.oninput=()=>{
     figura.style.height=alto.value+"px";
     document.getElementById("valto").textContent=alto.value;
 }
+
 ancho.oninput=()=>{
     figura.style.width=ancho.value+"px";
     document.getElementById("vancho").textContent=ancho.value;
 }
+
 color.onchange=()=>{
     figura.style.background=color.value;
 }
@@ -36,3 +56,12 @@ colorborde.onchange=()=>{
 tipoborde.onchange=()=>{
     figura.style.borderStyle=tipoborde.value;
 }
+
+/* SOMBRA */
+sombra.oninput=actualizarSombra;
+
+/* EJE X */
+ejex.oninput=actualizarSombra;
+
+/* EJE Y */
+ejey.oninput=actualizarSombra;
